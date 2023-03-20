1058ca1bc51528499c4481d65b52e2aa78fcfbba
Lu1s16 <78324884+Lu1s16@users.noreply.github.com>
2023-03-19 20:54:05 -0700

    Add files via upload

--- EDD_Proyecto1_Fase2/index.js
+++ EDD_Proyecto1_Fase2/index.js
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+const form = document.getElementById('login-form');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	const username = form.username.value;
+	const password = form.password.value;
+
+	if (username === 'usuario' && password === 'contraseña') {
+		message.innerText = 'Inicio de sesión exitoso!';
+		message.style.color = 'green';
+	} else {
+		message.innerText = 'Nombre de usuario o contraseña incorrectos.';
+		message.style.color = 'red';
+	}
+});
